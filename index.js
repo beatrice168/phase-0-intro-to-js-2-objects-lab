ac4fd9a5e2903f6d391842eed700ca5dd702fadc
beatrice <helgapaul389@gmail.com>
2023-03-21 08:39:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+let employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  // Create a new object by spreading the original employee object
+  const updatedEmployee = { ...employee };
+  
+  // Update the value for the specified key in the new object
+  updatedEmployee[key] = value;
+  
+  // Return the updated object
+  return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const { [key]: deletedValue, ...newEmployee } = employee;
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
